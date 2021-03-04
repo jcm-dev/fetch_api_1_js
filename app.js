b0472734c5c0afe6aeddc904935750aa86404ab0
Joshua <meyer.josh8@gmail.com>
2021-03-04 15:27:10 -0600

    Create getText function grabbing the text file with fetch and returning

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 document.getElementById('button1').addEventListener('click', getText);
 
+function getText(){
+  // fetch returen promises
+  // to get a response from a promise we are
+  // using .then()
+
+  // grabbing the txt file with fetch
+  // and then returning it with .then
+  // passing it in as 'res'
+  fetch('test.txt')
+    .then(function(res){
+      console.log(res);
+    });
+}
