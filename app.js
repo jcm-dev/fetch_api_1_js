481d6d5e7cc4b7d8e9e0f1e2afad53b358389621
Joshua <meyer.josh8@gmail.com>
2021-03-04 15:45:08 -0600

    Create getJson function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.getElementById('button1').addEventListener('click', getText);
 
 document.getElementById('button2').addEventListener('click', getJson);
 
+// GET LOCAL TXT DATA
 function getText(){
   // fetch returen promises
   // to get a response from a promise we are
@@ -20,6 +21,32 @@ function getText(){
       console.log(data);
       // log results in dom
       document.getElementById('ouput').innerHTML = data;
+    })
+    // catching the error
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+// GET LOCAL JSON DATA
+function getJson(){
+  // fetch returen promises
+  // to get a response from a promise we are
+  // using .then()
+
+  // grabbing the json file with fetch
+  // and then returning it with .then
+  // passing it in as 'res'
+  fetch('posts.json')
+    .then(function(res){
+
+      return res.json();
+    })
+    // returning the data from res
+    .then(function(data){
+      console.log(data);
+      // log results in dom
+      
     })
     // catching the error
     .catch(function(err){
